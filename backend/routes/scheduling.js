d8425c338da39ef90e42b511e1e4ee1fa25d1abc
Siddiha <fathimasiddika62@gmail.com>
2025-09-02 14:13:48 +0530

    changes

--- backend/routes/scheduling.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Get scheduling info
-router.get('/', (req, res) => {
-  res.json({ message: 'Scheduling service' });
-});
-
-module.exports = router;
